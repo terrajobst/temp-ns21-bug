d9d62884a2776a38516d1d88e8929108bedd286f
Immo Landwerth <immol@microsoft.com>
2019-02-25 12:03:11 -0800

    Add repro

---
+++ console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using lib;
+
+namespace console
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            await foreach (var x in Class1.Run(10))
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ lib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace lib
+{
+    public class Class1
+    {
+        public static async IAsyncEnumerable<int> Run(int max)
+        {
+            while (max > 0)
+            {
+                await Task.Delay(100);
+
+                yield return max;
+
+                max--;
+            }
+        }
+    }
+}
